4ed534cd0d2515e06e0c584ee9bdb82c3d9ac443
Pylance Issue Monitor <pylance-issue-monitor@users.noreply.github.com>
2026-05-30 02:07:40 -0700

    Add issue 6611 repro workspace

---
+++ pylance-issues/6611/mymodule.py
@@ -0,0 +1,13 @@
+class SomeClass:
+    """A simple class."""
+
+    pass
+
+
+def example():
+    """Example function.
+
+    See [`SomeClass`][mymodule.SomeClass] for more details.
+    Also see [`mymodule.SomeClass`][] for the full path version.
+    """
+    pass
